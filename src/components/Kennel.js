f55c89eb83d1bf17f8a273c41530267f753fe7bd
Michelle Johnson <bob1234girl@gmail.com>
2019-11-18 14:33:48 -0600

    added navbar

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -4,29 +4,12 @@ import './Kennel.css'
 import LocationCard from './location/LocationCard'
 import OwnerCard from './owner/OwnerCard'
 import EmployeeCard from './employee/EmployeeCard'
+import NavBar from './nav/NavBar'
 
 class Kennel extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <h2>Student Kennels<br />
-            <small>Loving care when you're not there.</small>
-          </h2>
-          <address>
-            Visit Us at the Nashville North Location
-            <br />500 Puppy Way
-          </address>
-        </div>
-        <div>
-          <AnimalCard />
-          <AnimalCard />
-          <AnimalCard />
-          <LocationCard />
-          <OwnerCard />
-          <EmployeeCard />
-        </div>
-      </div>
+      <NavBar />
     );
   }
 }
