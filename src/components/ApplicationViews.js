4533d3019284d6ceb2b9b1a434b63888909078a1
Michelle Johnson <bob1234girl@gmail.com>
2019-11-19 11:18:31 -0600

    followed along in chapter 4

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,7 @@ import AnimalCard from './animal/AnimalCard'
 import LocationCard from './location/LocationCard'
 import EmployeeCard from './employee/EmployeeCard'
 import OwnerCard from './owner/OwnerCard'
+import AnimalList from './animal/AnimalList'
 
 
 class ApplicationViews extends Component {
