70f244d8eeb26dc200833eccf915ae44c22d7621
Michelle Johnson <bob1234girl@gmail.com>
2019-11-21 13:40:25 -0600

    followed along in chapter

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,8 @@ import LocationList from './location/LocationList'
 import OwnerList from './owner/OwnerList'
 import AnimalList from './animal/AnimalList'
 import EmployeeList from './employee/EmployeeList'
+import AnimalDetail from './animal/AnimalDetail'
+
 
 
 class ApplicationViews extends Component {
@@ -16,9 +18,23 @@ class ApplicationViews extends Component {
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
-        <Route path="/animals" render={(props) => {
+        {/* Make sure you add the `exact` attribute here */}
+        <Route exact path="/animals" render={(props) => {
           return <AnimalList />
         }} />
+        <Route path="/animals/:animalId(\d+)" render={(props) => {
+          // Pass the animalId to the AnimalDetailComponent
+          return <AnimalDetail animalId={parseInt(props.match.params.animalId)} />
+        }} />
+
+        {/*
+        This is a new route to handle a URL with the following pattern:
+        http://localhost:3000/animals/1
+
+        It will not handle the following URL because the `(\d+)`
+        matches only numbers after the final slash in the URL
+        http://localhost:3000/animals/jack
+        */}
         <Route path="/locations" render={(props) => {
           return <LocationList />
         }} />
--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import "./Animal.css"
+import { Link } from "react-router-dom";
+
 
 class AnimalCard extends Component {
   render() {
@@ -11,6 +13,7 @@ class AnimalCard extends Component {
           </picture>
           <h2>Name: <span className="card-petname">{this.props.animal.name}</span></h2>
           <p>Breed: {this.props.animal.breed}</p>
+          <Link to={`/animals/${this.props.animal.id}`}><button>Details</button></Link>
           <button type="button" onClick={() => this.props.deleteAnimal(this.props.animal.id)}>Discharge</button>
         </div>
       </div>
